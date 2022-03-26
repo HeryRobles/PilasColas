7b4ed57962a9f43783f8f9b5e00d5b94df3d678c
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-03-26 12:31:58 -0700

    Validacion de Vacio y Lleno

---
+++ Arreglos/Pilas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Pilas
+    {
+        private string[] array;
+        private int max;
+        private int tope;
+        
+        public Pilas(int tamano)
+        {
+            array = new string[tamano];
+            max = array.Length -1;
+            tope = 0;
+           
+        }
+        private bool ValidaVacio()
+            //if (tope < 1)
+            //{
+            //return true;
+            //Console.Write("El Arreglo está Vacío");
+            //}
+            //else
+            //{
+            //return false;
+            //}
+        {
+            return (tope<1);
+        }
+        private bool ValidaLleno()
+        {
+            return (tope > max);
+        }
+    }
+}
