a5dbf6920d9c05a736b94a159ebf9b53d377b34d
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-03-28 09:18:56 -0700

    Se crearon los Métodos de Agregar, Eliminar y Validación vacio

--- Listas/Pilas.cs
+++ Listas/Pilas.cs
@@ -13,6 +13,26 @@ namespace Listas
         {
             lista = new List<string>();
         }
+
+        public void Agregar()
+        {
+            lista.Add(dato);
+        }
+
+        private bool ValidaVacio()
+        {
+            return (lista.Count == 0);
+        }
+
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("La Lista está Vacía");
+            }
+
+            lista.Remove(lista.Count - 1);
+        }
         
     }
 }
