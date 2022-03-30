304895ffcadd675da48db1089fcb13f5290d4dbc
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-03-30 13:27:19 -0700

    Atributos listas tipo cadena y creamos el constructor Colas

--- Listas/Colas.cs
+++ Listas/Colas.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Listas
 {
-    internal class Colas
+    public class Colas
     {
+        private List<string> lista;
+        public Colas()
+        {
+            lista = new List<string>();
+        }
+
     }
 }
