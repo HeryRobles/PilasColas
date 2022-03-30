c612a037517028eca211416c82e80d3d17642e51
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-03-30 13:37:46 -0700

    Se Agregaron los Métodos Validar Vacio, Add, Del y Print

--- Listas/Colas.cs
+++ Listas/Colas.cs
@@ -14,5 +14,46 @@ namespace Listas
             lista = new List<string>();
         }
 
+        public void Agregar(string dato)
+        {
+            lista.Add(dato);
+        }
+
+        private bool ValidaVacio()
+        {
+            return (lista.Count == 0);
+        }
+
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Lista Vacía");
+            }
+
+            lista.RemoveAt(0);
+        }
+
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+
+            if (ValidaVacio())
+            {
+                return "lista Vacía";
+            }
+
+            int listCount = lista.Count;  
+            for(int i = 0; i < listCount; i++)
+            {
+                if (i>0)
+                {
+                    datos += "\n";
+                }
+                datos += $"[{i}] - {lista[i]}";
+            }
+            return datos;
+        }
+
     }
 }
